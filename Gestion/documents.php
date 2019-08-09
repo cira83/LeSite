23553570e038300687008029b5463cd892f47adb
Patrick Gatt <patgat@MacBook-Air.local>
2019-08-09 15:11:54 +0200

    Update Document 3

--- Gestion/documents.php
+++ Gestion/documents.php
@@ -45,13 +45,14 @@
 		else {
 			$listeD = scandir($filename_of_elv);
 			if(count($listeD)>1){
-				echo("<ul>");
-				$fp = fopen("./files/$classe/_Documents/rep_$nom.txt","w");//Fichier qui liste le contenu du répertoire élève
+				//echo("<ul>");
+				$rep_nom = "./files/$classe/_Documents/rep_$nom.txt";
+				$fp = fopen($rep_nom,"w");//Fichier qui liste le contenu du répertoire élève
 				foreach($listeD as $name){
 					$part = explode(".", $name);
 					if($name=="index.htm") $part=0;
 					if($part[1]) {
-						echo("\n<li><a href=\"$filename_of_elv/$name\" class=\"no-under\">$name</a></li>");
+						//echo("\n<li><a href=\"$filename_of_elv/$name\" class=\"no-under\">$name</a></li>");
 						if($nb_ligne>0) fprintf($fp,"\n$name,$filename_of_elv/$name");
 						else {
 							$nb_ligne++;
@@ -59,8 +60,9 @@
 						}
 					}
 				}
-				echo("</ul>");
+				//echo("</ul>");
 				fclose($fp);
+				Dropbox("",$rep_nom);
 			}
 		}
 
