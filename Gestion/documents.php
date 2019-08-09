0d789a56f5b085363293d3c79a50faecfd45659c
Patrick Gatt <patgat@MacBook-Air.local>
2019-08-09 15:02:27 +0200

    Update documents.php

--- Gestion/documents.php
+++ Gestion/documents.php
@@ -2,22 +2,22 @@
 	include("./security.php");
 	include("../Dropbox.php");
 	include("../head1.html");
-	
+
 	$nom = $_SESSION['nom'];
 	$password = $_SESSION['password'];
 	$files = "./files/";
 	$classe = $_SESSION['laclasse'];
 	$repertoire_copies =  "./files/$classe/_Copies";
-	
+
 	$doc_elv_1 = $_GET['doc'];
 	if($doc_elv_1) {
 		$password_OK = $doc_elv_1;
 		$nom = $doc_elv_1;
 	}
-	
-	
-	
-?>		
+
+
+
+?>
 	<title>Documents <?php echo($nom);?></title>
 	<script type="text/javascript">
 	function login(){
@@ -30,11 +30,12 @@
 <!-- fin head -->
 	<body>
 		<img src="head.png"/>
-<?php	
+
+<?php
 	if($password_OK){
 		echo("<center><table><tr><td colspan=2><p class=\"titre\">Mes documents - $nom</p></td></tr></table></center>");
 		$filename_of_elv = "$repertoire_copies/$nom";
-		
+
 		$FilesDropBox = "./files/$classe/_Documents/$nom.txt";
 		if(file_exists($FilesDropBox)) {
 			Dropbox("",$FilesDropBox);
@@ -45,15 +46,22 @@
 			$listeD = scandir($filename_of_elv);
 			if(count($listeD)>1){
 				echo("<ul>");
+				$fp = fopen("./files/$classe/_Documents/rep_$nom.txt","w");//Fichier qui liste le contenu du répertoire élève
 				foreach($listeD as $name){
 					$part = explode(".", $name);
 					if($name=="index.htm") $part=0;
 					if($part[1]) echo("\n<li><a href=\"$filename_of_elv/$name\" class=\"no-under\">$name</a></li>");
+					if($nb_ligne>0) fprintf($fp,"\n$name,$filename_of_elv/$name");
+					else {
+						$nb_ligne++;
+						fprintf($fp,"$name,$filename_of_elv/$name");
+					}
 				}
 				echo("</ul>");
+				fclose($fp);
 			}
 		}
-		
+
 	}
 	else{
 		echo("<td colspan=2>Vous n'&ecirc;tes pas connect&eacute;(e) !!</td>$logout</tr></table>");
@@ -62,4 +70,4 @@
 
 <?php
 	include("../foot2.html");
-?>	
+?>
