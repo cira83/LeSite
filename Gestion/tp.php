a7b7822ce90d1bf969734dc4c709a47d332986e9
BTS CIRA 83 <53560015+cira83@users.noreply.github.com>
2019-08-09 09:05:48 +0200

    Add files via upload

---
+++ Gestion/tp.php
@@ -0,0 +1,24 @@
+﻿<?php
+	include("../head1.html");
+	include("../Dropbox.php");
+	$classe = $_COOKIE["laclasse"];
+	
+	if($B800) echo("<title>B800 - TP $classe</title>"); 
+	else echo("<title>TP $classe</title>"); 
+		
+?>	
+<!-- fin head -->
+<title>TP <?php echo($classe);?></title>
+	</head>
+	<body>
+		<img src="../../../../head.png"/>
+		<table><tr><td><p class="titre">TP <?php echo($classe);?></p></td></tr></table>
+
+<?php
+
+	Dropbox("","./files/$classe/_Sujets2TP/liste.txt");//Liste des documents nom,adresse web	
+
+
+	include("../foot2.html");
+?>	
+
