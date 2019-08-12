23ef15ec38e066b1057ce8e6345bc337b4024617
Patrick Gatt <patgat@MacBook-Air.local>
2019-08-12 08:42:16 +0200

    Update sujet2TP.php

--- Gestion/sujet2TP.php
+++ Gestion/sujet2TP.php
@@ -4,7 +4,21 @@
 	include("./haut.php");
 	include("../Dropbox.php");
 
-	Dropbox("Les TP du moment :","$Dir_TP/liste.txt");
+	Dropbox("$classe - Les sujets du moment :","$Dir_TP/liste.txt");
+	
+	$liste2 = scandir($Dir_TP);
+	$file = "$Dir_TP/allfiles.txt";
+	$fp = fopen($file, "w");
+	foreach($liste2 as $tp) {
+		if($pass) fprintf($fp, "\n$tp,$tp");
+		else {
+			$pass++;
+			fprintf($fp, "\n$tp,$tp");
+		}
+	}
+	fclose($fp);
+	
+	Dropbox("$classe - Tous les sujets :",$file);
 	
 	include("./bas.php");
 ?>
