abc4c57180f79a1e1f7d511bf76eeb31c6be6d44
Patrick Gatt <patgat@MacBook-Air.local>
2019-08-12 08:29:10 +0200

    1

--- Gestion/sujet2TP.php
+++ Gestion/sujet2TP.php
@@ -1,74 +1,10 @@
 <?php    	
 	$classe = $_COOKIE["laclasse"];
-	$laclassefile = "./files/$classe.txt";
+	$Dir_TP = "./files/$classe/_Sujets2TP";
 	include("./haut.php");
+	include("../Dropbox.php");
 
-	if($_GET[action]==1){
-		$filename = $_POST[filename];
-		$oui = "<form action=\"./files.php?action=2\" method=\"post\"> <input type=\"hidden\" name=\"filename\" value=\"$filename\"> <input type=\"submit\" value=\"OUI\"></form>";
-		$non = "<form action=\"./files.php\" method=\"post\"> <input type=\"submit\" value=\"NON\"></form>";
-		affiche("Voulez vous r&eacute;ellement supprimer $filename ? $oui$non");
-	}
-	
-	
-	if($_GET[action]==2){
-		$filename = $_POST[filename];
-		$directory = "./files/$classe/_Copies/_Poubelle";
-		if(!file_exists($directory)) {
-			mkdir($directory);
-			affiche("$directory crée");
-		}
-		$part = explode("/", $filename);
-		$name1 = $part[count($part)-1];
-		$newname = "$directory/$name1";
-		
-		rename($filename, $newname);
-		affiche("Le fichier $filename est");
-		affiche("déplacé vers $newname");
-
-	}
-	
-	for($i=0;$i<count($leleve);$i++){
-		$nomelv = $leleve[$i];
-		$directory = "./files/$classe/_Copies/$nomelv";
-		$listedesfichiers = scandir($directory);
-		$k=0;
-		for($j=0;$j<count($listedesfichiers);$j++) {
-			if(estfichier($listedesfichiers[$j])){
-				$supprimer = "<form action=\"./files.php?action=1\" method=\"post\"><input type=\"hidden\" name=\"filename\" value=\"$directory/$listedesfichiers[$j]\"><input type=\"submit\" value=\"Supprimer\"></form>";
-				$icone = icone2($listedesfichiers[$j]);
-				$listetext[$k]="<td><a href=\"$directory/$listedesfichiers[$j]\">$listedesfichiers[$j]</a></td><td width=\"20px\">$icone</td><td width=\"100px\">$supprimer</td>";
-				$k++;
-			}
-		}
-		//Le repertoire des réponses 20 decembre 2017
-		$directory = "./files/$classe/_Copies/$nomelv/rep";
-		$listedesfichiers = scandir($directory);		
-		for($j=0;$j<count($listedesfichiers);$j++) {
-			if(estfichier($listedesfichiers[$j])){
-				$supprimer = "<form action=\"./files.php?action=1\" method=\"post\"><input type=\"hidden\" name=\"filename\" value=\"$directory/$listedesfichiers[$j]\"><input type=\"submit\" value=\"Supprimer\"></form>";
-				$icone = icone2($listedesfichiers[$j]);
-				$listetext[$k]="<td><a href=\"$directory/$listedesfichiers[$j]\">$listedesfichiers[$j]</a></td><td width=\"20px\">$icone</td><td width=\"100px\">$supprimer</td>";
-				$k++;
-			}
-		}
-				
-		
-		if($k>0){
-			echo("<table><tr>");
-			echo("<td ROWSPAN=\"$k\" width=\"15%\">$nomelv</td>");
-			echo("$listetext[0]");
-			echo("</tr>");
-		}
-		for($j=1;$j<$k;$j++) {
-			echo("<tr>");
-			echo("$listetext[$j]");
-			echo("</tr>");	
-		}
-		if($k>0) echo("</table>");	
-	}
-	
-	
+	Dropbox("Les TP du moment :","$Dir_TP/liste.txt");
 	
 	include("./bas.php");
 ?>
