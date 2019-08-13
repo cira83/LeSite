4e850dc92439c2ac9b50db33e10b51fac7f45554
Patrick Gatt <patgat@MacBook-Air.local>
2019-08-13 11:11:02 +0200

    Update planning.php

--- Gestion/planning.php
+++ Gestion/planning.php
@@ -44,7 +44,10 @@
 		for($i=0;$i<count($ligne_planning);$i++){
 			$case = explode(":", $ligne_planning[$i]);
 			$deroulant = menu_deroulant($tableaudesepreuves,"epr$i",$case[0]);
-			//$deroulant = menu_deroulant(listedesTP("./files/$classe/_Sujets2TP/liste2.txt"),"epr$i",$case[0]);
+			//Verif 
+			$listeTPactifs[0]="0";
+			$listeTPactifs[1]="1";
+			$deroulant = menu_deroulant($listeTPactifs,"epr$i",$case[0]);
 
 			//Recherche du fichier
 			for($l=0;$l<count($lepreuve1);$l++){
