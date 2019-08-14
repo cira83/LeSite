3ebba28fe982bdc9dbd649ea0d3ea1914c8693bd
Patrick Gatt <patgat@MacBook-Air.local>
2019-08-14 10:19:00 +0200

    Update lesfonctions.php

--- Gestion/lesfonctions.php
+++ Gestion/lesfonctions.php
@@ -683,7 +683,7 @@
 	}
 
 	//LISTE DES ELEVES DE LA CLASSE
-	$deroulant3 = "<SELECT name=\"elv\" id=\"elv\">";
+	$deroulant3 = "<SELECT name=\"elv\" id=\"elv\"><OPTION>----</OPTION>";
 	$listedeseleves = "&Eacute;l&egrave;ves : ";
 	$laclassefile = "./files/$classe.txt";
 	$fichierdenom = fopen($laclassefile, "r"); 
