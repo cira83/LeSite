28918c3dfc9a6f5acb65bb66b6eb0313c7680eea
Patrick Gatt <patgat@MacBook-Air.local>
2019-09-17 16:52:26 +0200

    img1

---
+++ Gestion/files/CIRA2/_Sujets2TP/img1/TP1.php
@@ -0,0 +1,32 @@
+<h2>I. Préparation du travail (5pt)</h2>
+<ol>
+<li>Compléter le schéma TI avec l’instrumentation et les liaisons nécessaires à la conception de la boucle de régulation.(2pt)</li>
+<li>Quel est le nom de la grandeur réglée ? (0.5pt)</li>
+<li>Quel est le principe utilisé pour mesurer la grandeur réglée ? (0.5pt)</li>
+<li>Quelle est la grandeur réglante ? (0.5pt)</li>
+<li>Donner une grandeur perturbatrice. (0.5pt)</li>
+<li>Etablir le schéma de câblage complet en tenant compte de la nature des signaux utilisés. Prévoir les convertisseurs, alimentations, générateurs nécessaires. Faire apparaître les polarités. (1pt)</li>
+</ol>
+
+<h2>II. Etude du procédé (7pt)</h2>
+<ol>
+<li>Paramétrer les entrées-sorties de votre régulateur en fonction de la nature des signaux utilisés. (1pt)</li>
+<li>Tracer la caractéristique statique de votre procédé. On prendra au moins 6 mesures (3 pour les régulations de température et niveau). (1pt)</li>
+<li>En déduire le gain statique du procédé autour du point de fonctionnement. (1pt)</li>
+<li>En déduire le sens d'action à régler sur le régulateur. (1pt)</li>
+<li>Déterminer le modèle de Broïda du procédé, en faisant un échelon de 10% autour du point de fonctionnement. (3pt)</li>
+</ol>
+
+<h2>III. Etude du régulateur (3pt)</h2>
+<ol>
+<li>Déterminer la structure interne (parallèle, série ou mixte) du correcteur PID utilisé par Lintools. (1.5pt)</li>
+<li>En déduire le réglage du régulateur en utilisant le tableau de réglage fourni dans le cours. (1.5pt)</p>
+</ol>
+
+<h2>IV. Performances et optimisation (5pt)</h2>
+<ol>
+<li>Programmer votre régulateur pour assurer le fonctionnement de la régulation.(1pt)</li>
+<li>Mesurer les performances de votre régulation en réponse à un échelon de consigne de 10%. On mesurera le temps de réponse à 10%, la valeur du premier dépassement et la précision relative. (1.5pt)</li>
+<li>Améliorer votre réglage pour réduire au maximum la valeur du temps de réponse. On donnera le nom et la valeur des paramètres modifiés. (1pt)</li>
+<li>Mesurer à nouveau les performances de votre régulation, comparer les avec celles obtenues à la question précédente. (1.5pt)</li>
+</ol>
