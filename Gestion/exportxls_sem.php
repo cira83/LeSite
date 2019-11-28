f8920cd56cdf8c04749c2363ba9538faa45178d5
Patrick Gatt <patgat@MacBook-Air.local>
2019-11-28 10:57:58 +0100

    Update exportxls_sem.php

--- Gestion/exportxls_sem.php
+++ Gestion/exportxls_sem.php
@@ -4,13 +4,34 @@
 	$sem = $_GET[sem];
 	$classe = $_GET[classe];
 
-	$txt_output = "$classe Semestre $sem";
+	$directory = "./files/$classe";
+	$repertoires = scandir($directory);
+	foreach($repertoires as $rep) {
+		if(!strpos(" $rep", "_")&&strpos($rep, $sem)) {
+			$filename = "./files/$classe/_$rep.txt";
+			if(file_exists($filename)) {
+				$fp = fopen($filename, "r");
+				$tabnote = fgets($fp);
+				$note = explode(":", $tabnote);
+				$tabnom =  fgets($fp);
+				$nom = explode(":", $tabnom);
+				fclose($fp);
+				
+				$couple[0] .= "nom\t$rep\t";
+				$nb2note = count($nom);
+				for($i=0;$i<$nb2note;$i++){
+					$note[$i]=strtr($note[$i], ".", ",");
+					$note[$i]=rtrim($note[$i]);
+					$couple[1+$i] .= "$nom[$i]\t$note[$i]\t";
+				}
+			}
+		}	
+	}
+	$nb2note = count($couple);
+	for($i=0;$i<$nb2note;$i++) $txt_output.="$couple[$i]\n";
 	
-	
-	
-	//echo($txt_output);
-	
-	header("Content-disposition: attachment; filename=$part[0]_" . date("d_m").".txt");
+		
+	header("Content-disposition: attachment; filename=$classe" ." Semestre $sem"." le ". date("d_m").".txt");
 	print $txt_output;
 	exit;
 ?>
