ec28674733048fe94fa84c959c540bac4059c98a
Patrick Gatt <patgat@MacBook-Air.local>
2019-11-28 10:02:04 +0100

    toto

--- Gestion/exportxls_sem.php
+++ Gestion/exportxls_sem.php
@@ -1,26 +1,10 @@
 <?php
 	header ("Content-type: text/plain");//Pour le faire considérer par le système comme un fichier texte et non html
 	
-	$filesave = $_GET[filesave];
-	$file = $_GET[file]; $part = explode(".", $file);
-	$moy = $_GET[moy];
+	$sem = $_GET[sem];
+	$classe = $_GET[classe];
+
 	
-	if(file_exists($filesave)){
-		$fp = fopen($filesave, "r");
-		$tabnote = fgets($fp);
-		$note = explode(":", $tabnote);
-		$tabnom =  fgets($fp);
-		$nom = explode(":", $tabnom);
-		fclose($fp);
-	}
-	
-	$txt_output = "Nom\t$part[0]\n";
-	
-	$nb2note = count($nom);
-	for($i=0;$i<$nb2note;$i++){
-		$note[$i]=strtr($note[$i], ".", ",");
-		$txt_output .= "$nom[$i]\t$note[$i]\n";
-	}
 	
 	
 	echo($txt_output);
