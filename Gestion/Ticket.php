8456832abc099a34f9ec80320f7e1c389b902f88
Patrick Gatt <patgat@MacBook-Air.local>
2019-12-16 12:19:03 +0100

    Ticket Plus

--- Gestion/Ticket.php
+++ Gestion/Ticket.php
@@ -89,25 +89,10 @@
 		}
 	}
 
-
+	if($tp) include("Ticket_plus.php");
 ?>
 
 
-<h1>Ajouter un ticket</h1>
-<form method="post" action="Ticket.php?sujet=<?php echo($tp);?>">
-	<table>
-		<tr class="orange">
-			<td>Sujet de TP</td>
-			<td>Indications</td>
-			<td></td>
-		</tr>
-		<tr>
-			<td><?php echo($tp);?></td>
-			<td><input type="text" size="100" name="indications"></td>
-			<td><input type="hidden" name="drap" value="1"><input type="submit"></td>
-		</tr>
-	</table>
-</form>
 
 <h1>Liste des tickets</h1>
 <table>
